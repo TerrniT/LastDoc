424133e8f49913b6d4879e53ae356468fc24bd49
TerrniT <terrnit.megaman@gmail.com>
2023-02-05 17:29:51 +0500

    docs(eslint): test eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import {Flex} from "@chakra-ui/react"
+import Content from "./components/Content"
+import Sidebar from "./components/Sidebar"
 
 export default function App() {
   return (
     <Flex h="100vh">
-
+      <Sidebar/>
+      <Content/>
     </Flex>
   
   )
