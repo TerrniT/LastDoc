0c12b7ceee96a8181329696748f1ecf220a679ce
TerrniT <terrnit.megaman@gmail.com>
2023-02-05 17:42:38 +0500

    Revert "docs(eslint): test eslint"
    This reverts commit 424133e8f49913b6d4879e53ae356468fc24bd49.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import {Flex} from "@chakra-ui/react"
-import Content from "./components/Content"
-import Sidebar from "./components/Sidebar"
 
 export default function App() {
   return (
     <Flex h="100vh">
-      <Sidebar/>
-      <Content/>
+
     </Flex>
   
   )
