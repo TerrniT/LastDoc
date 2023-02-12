7b52bc12b01fac9b44eebb2224719d8e4f94dbea
TerrniT <terrnit.megaman@gmail.com>
2023-02-12 17:28:56 +0500

    feat(ui): add sidebar variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
 import { Button } from "./components/atoms/button";
+import Sidebar from "./components/Sidebar";
 
 export default function App() {
-	return (
-		<div className='h-screen bg-white mx-auto mt-10 flex flex-col gap-4 w-24'>
-			<Button variant={"default"}>Button</Button>
-			<Button variant={"destructive"}>Button</Button>
-			<Button variant={"ghost"}>Button</Button>
-			<Button variant={"link"}>Button</Button>
-			<Button variant={"outline"}>Button</Button>
-			<Button variant={"subtle"}>Button</Button>
-
-			<Button variant={"default"} size='default'>
-        Button
-			</Button>
-			<Button variant={"default"} size='lg'>
-        Button
-			</Button>
-			<Button variant={"default"} size='sm'>
-        Button
-			</Button>
-		</div>
-	);
+	return <Sidebar />;
 }
