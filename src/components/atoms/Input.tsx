3bd955c2ec117f6f1d6ea36ac1d3a9e590aae6e7
TerrniT <terrnit.megaman@gmail.com>
2023-02-22 17:52:18 +0500

    fix(pre-commit): fix husky

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -19,7 +19,9 @@ const Input = (props: Props) => {
 			<input
 				type='text'
 				id='email-address-icon'
-				className={"bg-white border  w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  h-11 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
+				className={
+					"bg-white border  w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  h-11 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				}
 				placeholder={props.placeholder}
 			/>
 		</div>
